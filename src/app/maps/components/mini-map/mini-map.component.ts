025a9803b91ee1df4f5a074a62ebf81bd4c54f72
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-29 08:39:21 +0200

    ✨ show mini map as img in card

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
-  styleUrl: './mini-map.component.css'
+  styleUrl: './mini-map.component.css',
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  @ViewChild('map')
+  public divMap?: ElementRef;
 
+  @Input()
+  lngLat?: [number, number];
+
+  ngAfterViewInit(): void {
+    if (!this.divMap?.nativeElement) throw new Error('Map HTML not found');
+    if (!this.lngLat) throw Error("LngLat can't be null");
+
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false,
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
 }

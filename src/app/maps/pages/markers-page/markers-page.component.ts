00fa09aac5fde9e905ed8205c07eddf91272a3c6
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-29 07:58:28 +0200

    ✨ add marker on click

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
-  styleUrl: './markers-page.component.css'
+  styleUrl: './markers-page.component.css',
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild('map')
+  public divMap?: ElementRef;
 
+  public map?: Map;
+  public currentCenter: LngLat = new LngLat(
+    -74.08213790000212,
+    4.643017390551975
+  );
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) {
+      throw new Error('HTML element was not found');
+    }
+
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.currentCenter,
+      zoom: 13,
+    });
+
+    // const markerHTML = document.createElement('div');
+    // markerHTML.innerHTML = 'Edgar Vergara';
+
+    // const marker = new Marker({
+    //   color: 'red',
+    //   element: markerHTML,
+    // })
+    //   .setLngLat(this.currentCenter)
+    //   .addTo(this.map);
+  }
+
+  createMarker() {
+    if (!this.map) return;
+
+    const lngLat = this.map.getCenter();
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    this.addMarker(lngLat, color);
+  }
+
+  addMarker(lngLat: LngLat, color: string = 'red') {
+    if (!this.map) return;
+
+    const marker = new Marker({
+      color,
+      draggable: true,
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
+  }
 }

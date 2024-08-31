b1bf98f355de51627cadde3975fceb91cf3388f8
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-31 06:12:03 +0200

    ✨ standalone counter component

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   standalone: true,
-  // imports: [CommonModule],
+  imports: [CounterAloneComponent],
   templateUrl: './alone-page.component.html',
   styleUrl: './alone-page.component.css',
 })
